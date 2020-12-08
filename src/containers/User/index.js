6fe7009490a48d65b52f6a397e693925b11bbb21
Dagmara <przygocka.dagmara@gmail.com>
2020-12-08 20:46:52 +0100

    View user data

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react';
-import UserData from './../../api/users.js';
+import request from "../../utils/request";
+import 'bootstrap/dist/css/bootstrap.css';
 
 
  class UserData extends React.Component{
     constructor(props){
+        super(props);
         this.retriveUser = this.retriveUser.bind(this);
 
         this.state ={
         user : {
             "userId": -1,
-            "firstName": "",
-            "lastName": "",
-            "email": "",
+            "firstName": " ",
+            "lastName": " ",
+            "email": " ",
             "age": -1,
-            "gender": "",
-            "phone": ""
+            "gender": " ",
+            "phone": " "
         }
     }}
     
@@ -24,15 +26,22 @@ import UserData from './../../api/users.js';
 
     retriveUser(user_id){
         //this.setState({user : UserData.getUser(user_id)});
-        User.getUser(user_id).then(response => this.setState({ user:{
-            userId : response.data.user_id,
-            firstName : response.data.firstName,
-            lastName : response.data.lastName,
-            email : response.data.email,
-            age : response.data.age,
-            gender : response.data.gender,
-            phone : response.data.phone
-        }})).catch(error => {
+        request(`http://localhost:8181/api/profiles/user/`+ user_id, { 
+            method : 'get'
+        }).then(response => {
+            console.log(response);
+            this.setState({ 
+            user:{
+                userId : response.userId,
+                firstName : response.firstName,
+                lastName : response.lastName,
+                email : response.email,
+                age : response.age,
+                gender : response.gender,
+                phone : response.phone
+            }
+            })
+        }).catch(error => {
             console.log('error: ', error)
             throw error;
         })
@@ -42,27 +51,21 @@ import UserData from './../../api/users.js';
         return (
             <div className='container'>
                 <div className='row'>
-                    <div className='col-md-12'>
-                        <h1>{this.state.user.firstName + " " + this.state.user.lastName}</h1>
-                    </div>
-                    <div class Name='row'>
                         <div className='col-md-6'>
-                            <table>
-                                <tr>
-                                    {Object.key(this.state.user).forEach(key => {
-                                        <td>{key}</td>
-                                    })}
-                                </tr>
-                                <tr>
-                                {Object.key(this.state.user).forEach(key => {
-                                        <td>{user.key}</td>
-                                    })}
-                                </tr>
+                            <table className='table table-hover'>
+                                <tbody>
+                                        {Object.keys(this.state.user)
+                                        .map(name => { return(
+                                        <tr>
+                                            <th scope="row"></th>
+                                            <td>{name}</td>
+                                            <td>{this.state.user[name]}</td>
+                                        </tr>);
+                                        })} 
+                                </tbody>
+                                
                             </table>
                         </div>
-                    </div>
-                    <div></div>
-                    <div></div>
                 </div>
             </div>
         );
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,12 +1,12 @@
 import { all } from 'redux-saga/effects'
 import profile from './profile'
 import teams from './teams'
-import user from './user'
+
 
 const allSagas = [
     teams(),
     profile(),
-    user()
+
 ]
 
 export default function* rootSaga(){
